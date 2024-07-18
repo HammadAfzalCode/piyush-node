cd9988acdcce4c8e0363ef303aaf057d6504f5d6
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-07-18 00:11:32 -0700

    sync and async file read

--- File-handling/file.js
+++ File-handling/file.js
@@ -3,3 +3,14 @@ const fs = require("fs");
 fs.writeFileSync("./test.txt", "Hey there"); // synchronous file writing
 
 fs.writeFile("./async.txt", "hello there async", (err) => {});
+
+const result = fs.readFileSync("./async.txt", "utf-8"); // here utf is encoding language
+console.log(result);
+
+fs.readFile("./test.txt", "utf-8", (err, result) => {
+  try {
+    console.log(result);
+  } catch (err) {
+    console.log("Error", err);
+  }
+});
