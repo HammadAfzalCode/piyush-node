c9c79e2184abb8c92a04b39f759aeaa3b0f6cbcb
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-07-18 00:17:55 -0700

    file append

--- File-handling/file.js
+++ File-handling/file.js
@@ -1,16 +1,18 @@
 const fs = require("fs");
 
-fs.writeFileSync("./test.txt", "Hey there"); // synchronous file writing
+// fs.writeFileSync("./test.txt", "Hey there"); // synchronous file writing
 
-fs.writeFile("./async.txt", "hello there async", (err) => {});
+// fs.writeFile("./async.txt", "hello there async", (err) => {});
 
-const result = fs.readFileSync("./async.txt", "utf-8"); // here utf is encoding language
-console.log(result);
+// const result = fs.readFileSync("./async.txt", "utf-8"); // here utf is encoding language
+// console.log(result);
 
-fs.readFile("./test.txt", "utf-8", (err, result) => {
-  try {
-    console.log(result);
-  } catch (err) {
-    console.log("Error", err);
-  }
-});
+// fs.readFile("./test.txt", "utf-8", (err, result) => {
+//   try {
+//     console.log(result);
+//   } catch (err) {
+//     console.log("Error", err);
+//   }
+// });
+
+fs.appendFileSync("./test.txt", "hey hammad\n");
